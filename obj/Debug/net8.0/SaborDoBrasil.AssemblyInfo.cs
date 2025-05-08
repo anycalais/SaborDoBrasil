1b025422b20c8f2bb55ea46c02f8f91a635ea133
Kauany Calais <kauanycalais10@gmail.com>
2025-05-08 16:03:01 -0300

    adicionando cadastro

--- obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab84b1e1891cb6059f3be255a6246248b4a91eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5972597d63adc87608e78de76ced0526fb04e258")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
